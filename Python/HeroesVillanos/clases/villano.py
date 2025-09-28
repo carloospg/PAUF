9b0b7d47108ea8982ca2043e221933b454ffaef7
carloospg <carlosperezgosalvez@gmail.com>
2025-09-28 17:26:21 +0200

    creacion clase villano

--- Python/HeroesVillanos/clases/villano.py
+++ Python/HeroesVillanos/clases/villano.py
@@ -1,4 +1,13 @@
 from persona import Persona
+import random
 
 class Villano(Persona):
-    pass
+    atributos = ["Chatgepeteador", "Entregador Tardio", "Ausencias", "Hablador"]
+
+    def __init__(self, nombre, ap1, ap2, fechaNac, id):
+        super().__init__(nombre, ap1, ap2, fechaNac, id)
+        self.cualidades = {atrib: random.randint(0, 100) for atrib in Villano.atributos}
+        self.puntuacionTotal = sum(self.cualidades.values()) / len(self.cualidades)
+
+    def __str__(self):
+        return super().__str__() + f" Cualidades: " + self.cualidades
